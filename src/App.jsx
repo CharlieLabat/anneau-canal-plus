1f6c5ccf4964c27cb2aad5a91091a872ad186505
Charlie LABAT <charlielabat17@gmail.com>
2025-12-14 18:05:53 +0100

    Dépot initiale

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+import './App.scss'
+
+function App() {
+
+
+  return (<section className='zonePrincipal'>
+    <h1 className='titrePrincipal'>Une nouvelle ère</h1>
+  </section> )
+}
+
+export default App
