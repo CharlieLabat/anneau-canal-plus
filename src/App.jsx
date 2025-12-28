3c5015d5e1df8493383edba377270bc431382a25
Charlie LABAT <charlielabat17@gmail.com>
2025-12-28 18:41:54 +0100

    Bas de page et début création anneau

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
-
 import './App.scss'
+import Anneau from './components/Anneau'
 
 function App() {
 
 
-  return (<section className='zonePrincipal'>
+  return (
+  <>
+  <section className='zonePrincipal'>
     <h1 className='titrePrincipal'>Une nouvelle ère</h1>
-  </section> )
+  </section>
+  <Anneau></Anneau>
+  <footer className='basPage'>
+    <p className='basPage__partie'></p>
+    <p className='basPage__partie'></p>
+    <p className='basPage__partie'></p>
+  </footer>
+  </> )
 }
 
 export default App
--- src/components/Anneau.jsx
+++ src/components/Anneau.jsx
@@ -1,5 +1,7 @@
 
 const Anneau = () => {
-
+ return <section className="anneau">
+    <h2 className="anneau__texte">Coucou</h2>
+ </section>
 }
 export default Anneau
