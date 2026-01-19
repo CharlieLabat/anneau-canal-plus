8d3a2da9372cf3f40cc91fd9561029796d896b4a
Charlie LABAT <charlielabat17@gmail.com>
2026-01-19 15:21:19 +0100

    Début animation anneau

--- src/components/Anneau.jsx
+++ src/components/Anneau.jsx
@@ -1,7 +1,7 @@
 
 const Anneau = () => {
  return <section className="anneau">
-    <h2 className="anneau__texte">Coucou</h2>
+    <h2 className="anneau__texte">COUCOU +</h2>
  </section>
 }
 export default Anneau
